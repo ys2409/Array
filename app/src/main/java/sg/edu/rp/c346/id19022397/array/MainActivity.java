39fb4ffdd63b683ee81a03b463907a9fdcab0f50
ys2409 <19022397@myrp.edu.sg>
2020-06-09 14:08:47 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19022397/array/MainActivity.java
@@ -0,0 +1,41 @@
+package sg.edu.rp.c346.id19022397.array;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    String[] fruits;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.setTitle("Array");
+
+        fruits = new String[3];
+        fruits[0] = "apple";
+        fruits[1] = "banana";
+        fruits[2] = "cherry";
+
+        tv = findViewById(R.id.textView);
+
+        String text = "Fruits\n";
+        text += "=====\n";
+
+        for(int i = 0; i<fruits.length; i++){
+            text += fruits[i] + "\n";
+        }
+
+
+
+
+
+
+
+    }
+}
+
